e3988c8e8528a7a4c1860897e318945b0d7413b9
jpickler <sirzymok@gmail.com>
2023-11-09 15:00:39 -0500

    individual card is operational, moving to bonus items

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
-const state = [
-  puppies = {},
-];
+const state = {
+   puppies: []
+};
+
 
 
 const main = document.querySelector(`main`);
 
 const getPuppyDetails = (puppyIdentifier) => {
 
-  const individualPuppy = [];
-  console.log(`this is state.puppies  ${state.puppies}`)
+  let individualPuppy = [];
+  //console.log(`this is puppyIdentifier `,puppyIdentifier)
+
   for (i = 0; i < state.puppies.length; i++) {
-    if (state.puppies[i].id === puppyIdentifier) {
-      console.log(state.puppies[i]);
-      individualPuppy.slice(state.puppies[i]);
+    //console.log(state.puppies[i].id);
+    if (puppyIdentifier == state.puppies[i].id) {
+      individualPuppy =state.puppies[i];
     };
   };
-  console.log(individualPuppy);
-  console.log(`puppy name is: ${individualPuppy.name}`);
+  //console.log(individualPuppy);
+  //console.log(`puppy name is: `, individualPuppy.name);
 
 
   // ******  create individual puppy card
   main.innerHTML = ``;
   const puppyCard = document.createElement(`div`);
+  puppyCard.setAttribute("style", "display: flex; justify-content: space-between; border-style: solid; border-color:black;height: 300px; width:500px; margin:10px;");
   main.appendChild(puppyCard);
 
 
 
   cardData = `
+    <div style="align-self: center">
     <h2 id="name"> Name: ${individualPuppy.name} </h2>
     <h2 id="breed"> Breed: ${individualPuppy.breed} </h2>
     <h2 id="status"> Status: ${individualPuppy.status} </h2>
-    <img id="image" />
-    <button type="button", id="backButton"> Back to Roster </button>
+    </div>
+    <img id="image" src="${individualPuppy.imageUrl}" width=200px />
     `;
+   // <button type="button", id="backButton"> Back to Roster </button>
+
+
 
   puppyCard.innerHTML = cardData;
-  const backButton = document.querySelector(`#backButton`);
+  //const backButton = document.querySelector(`#backButton`);
   //console.log(backButton);
+  const backButton=document.createElement(`button`);
+  backButton.textContent=`Return to Roster`;
+  main.appendChild(backButton);
   backButton.addEventListener(`click`, () => {
-    console.log(`button pushed`);
+    //console.log(`button pushed`);
     getPuppyData();
   });
 
@@ -48,13 +58,13 @@ const getPuppyDetails = (puppyIdentifier) => {
 
 const getPuppyData = async (event) => {
   const puppyDataResponse = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2310-fsa-et-web-ft-sf/players`);
-  console.log(`this is the response to the fetch: ${puppyDataResponse}`);
+  //console.log(`this is the response to the fetch: ${puppyDataResponse}`);
   const puppyDataJson = await puppyDataResponse.json();
-  console.log(`this is puppyDataJSON should be in json format:  ${puppyDataJson}`);
+  //console.log(`this is puppyDataJSON should be in json format: `, puppyDataJson);
   const puppyData = puppyDataJson.data.players;
-  console.log(`this is puppyData after Json command and down to the players:  ${puppyData}`);
+  //console.log(`this is puppyData after Json command and down to the players: `, puppyData);
   state.puppies = puppyData.slice();
-  console.log(`this is state.puppies after response  ${state.puppies}`);
+  //console.log(`this is state.puppies after response: `, state.puppies);
 
 
 
@@ -68,7 +78,7 @@ const getPuppyData = async (event) => {
 
     const rosterListDisplay = document.createElement(`ul`);
     main.appendChild(rosterListDisplay);
-    console.log(`this is state.puppies just before li is created  ${state.puppies}`)
+    //console.log(`this is state.puppies just before li is created `,state.puppies)
 
     state.puppies.forEach(element => {
       const rosterListItems = document.createElement(`li`);
@@ -82,7 +92,7 @@ const getPuppyData = async (event) => {
     clickableListItmes.forEach((clickableListItem) => {
       clickableListItem.addEventListener(`click`, (event) => {
         //getPuppyDetails(event.target.innerText);
-        console.log(`this is state.puppies just before going to single puppy ${state.puppies}`)
+        //console.log(`this is state.puppies just before going to single puppy `,state.puppies)
         getPuppyDetails(event.target.id);
       }); // end of clickableListItems add eventlistener
     }); // end of clickablelistitems
